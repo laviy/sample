18c3e36f90f410d4f2c3ef1ebedb80b9e6e4a4e7
laviy <yonatan_lavi@asl.org>
2014-10-14 08:49:53 +0100

    my world

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+  
+   public static void main(String[] args) {
+   
+      System.out.println("Hello Class");
+      System.out.print("4/2 is: ");
+      System.out.println(2.0/4);
+
+   }
+
+}
